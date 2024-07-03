b69b16d1212f49727290e74c1614fa47f21626fb
ellsurge <iorsasesesugh@gmail.com>
2024-07-03 07:25:34 +0100

    fix: removed props with child

--- src/components/Atoms/RowAtom/RowAtom.tsx
+++ src/components/Atoms/RowAtom/RowAtom.tsx
@@ -1,13 +1,12 @@
-import type { ReactNode } from "react";
+import type { PropsWithChildren, ReactNode } from "react";
 import "./custom.css";
 
 type Props = {
   // Add props here
   className?: string | undefined;
-  children: ReactNode;
 };
 
-export const RowAtom = (props: Props) => {
+export const RowAtom = (props: PropsWithChildren<Props>) => {
   return (
     <div className={`${props.className} flex flex-wrap `}>{props.children}</div>
   );
